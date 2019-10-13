35ac893de4ab8268de580f7cc83e2813e0cb8295
Andrea T <tostonar@gmail.com>
2019-10-13 19:00:27 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(games) {
+  let win = games.find( game => game.result === 'W');
+  if (win) {
+     return win.year;
+  } else {
+    return undefined;
+  }
+}
